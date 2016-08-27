56da4a1aa60caf17b2c94b2b0701ec2a8f0cd964
BrianMacIntosh <bmacbrian@pacbell.net>
2016-08-27 13:14:32 -0700

    merged

--- index.bundle.js
+++ index.bundle.js
@@ -9,19 +9,13 @@ GameEngine = new bmacSdk.Engine("canvasDiv");
 
 // add objects to the engine
 GameEngine.addObject(require("./src/game/conversation.js"));
-<<<<<<< HEAD
-GameEngine.addObject(require("./src/game/sample.js"));
+
 window.Inventory = require("./src/game/inventory.js")
 GameEngine.addObject(Inventory);
-
-=======
 
 //HACK:
 window.SceneManager = require("./src/game/scenemanager.js");
 GameEngine.addObject(window.SceneManager);
-
-GameEngine.addObject(require("./src/game/inventory.js"));
->>>>>>> daf3736e9e9aa3de9db93bda3bce178fccd97086
 
 // that's it!
 
